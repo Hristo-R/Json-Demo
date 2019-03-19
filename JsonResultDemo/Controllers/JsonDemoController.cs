5c7ab1a9353a603c920a4287156ccdd873ce1e1b
Hristo-R <hristo.razpopov@abv.bg>
2019-03-19 16:32:40 +0200

    added controller

---
+++ JsonResultDemo/Controllers/JsonDemoController.cs
@@ -0,0 +1,20 @@
+﻿namespace JsonResultDemo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class JsonDemoController : Controller
+    {
+        /// <summary>  
+        /// Welcome Note Message  
+        /// </summary>  
+        /// <returns>In a Json Format</returns>  
+        public JsonResult WelcomeNote()
+        {
+            bool isAdmin = false;
+            //TODO: Check the user if it is admin or normal user, (true-Admin, false- Normal user)  
+            string output = isAdmin ? "Welcome to the Admin User" : "Welcome to the User";
+
+            return Json(output);
+        }
+    }
+}
