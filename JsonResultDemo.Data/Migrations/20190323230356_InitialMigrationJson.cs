6bf6b1ac2835081d433479a5665df4cfe69dedb8
Hristo-R <hristo.razpopov@abv.bg>
2019-03-26 21:37:15 +0200

    Migrations

---
+++ JsonResultDemo.Data/Migrations/20190323230356_InitialMigrationJson.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JsonResultDemo.Data.Migrations
+{
+    public partial class InitialMigrationJson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
