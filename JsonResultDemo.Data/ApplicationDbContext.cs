67b5b075379180d0d755e7ba3941a19a19f86f9e
Hristo-R <hristo.razpopov@abv.bg>
2019-03-26 21:43:10 +0200

    Added Sample.cshtml

---
+++ JsonResultDemo.Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace JsonResultDemo.Data
+{
+    using JsonResultDemo.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserModel> UserModels { get; set; }
+    }
+}
